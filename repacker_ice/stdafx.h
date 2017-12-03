4b8d8c369175e15bffd87c9c3d5ac2b6a790d77b
Logue <logue@hotmail.co.jp>
2017-12-03 21:25:33 +0900

    ice.exeで圧縮時にアプリがクラッシュする問題を修正。

--- repacker_ice/stdafx.h
+++ repacker_ice/stdafx.h
@@ -1,10 +1,6 @@
 ﻿// stdafx.h : 標準のシステム インクルード ファイルのインクルード
-// ファイル、または
-// 参照回数が多く、かつあまり変更されない、プロジェクト専用のインクルード
-// ファイル
-// を記述します。
-//
-
+// ファイル、または参照回数が多く、かつあまり変更されない、
+// プロジェクト専用のインクルードファイルを記述します。
 #pragma once
 #define WIN32_LEAN_AND_MEAN
 
@@ -23,14 +19,27 @@
 
 #include <iostream>
 
+// Macro
 #ifndef _countof
 #define _countof(a) (sizeof(a) / sizeof(a[0]))
 #endif
 
-#define VERSION "1.30"
+#define BitsCount(val) (sizeof(val) * CHAR_BIT)
+#define Shift(val, steps) (steps % BitsCount(val))
+#define ROL(val, steps)                                                        \
+  ((val << Shift(val, steps)) | (val >> (BitsCount(val) - Shift(val, steps))))
+#define ROR(val, steps)                                                        \
+  ((val >> Shift(val, steps)) | (val << (BitsCount(val) - Shift(val, steps))))
+
+inline unsigned int bswap(unsigned int v) {
+  return (v << 0x18) | ((v & 0x0000ff00) << 0x08) | ((v & 0x00ff0000) >> 0x08) |
+         (v >> 0x18);
+}
+
+// Version
+
+#define VERSION "1.31"
 #define LIST_FILENAME "FileList.txt"
 #define ICE_VERSION 4
 #define CRYPT_KEY 0xCD50379E
 #define SIGNATURE 0x00454349
-
-// TODO: プログラムに必要な追加ヘッダーをここで参照してください
